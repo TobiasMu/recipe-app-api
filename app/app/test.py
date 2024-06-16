3d92f8d463ce785b799d2d88ab19955a0eddf12c
TobiasMu <64594392+TobiasMu@users.noreply.github.com>
2024-06-16 17:10:01 +0200

    database

--- app/app/test.py
+++ app/app/test.py
@@ -5,16 +5,15 @@ sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CaclTests(SimpleTestCase):
     """test calc"""
-    
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
-     
+
     def test_subtract_numbers(self):
         res = calc.subtract(15, 10)
-        
         self.assertEqual(res, 5)
-        
