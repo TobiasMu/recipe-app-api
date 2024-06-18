8684c1c52c9a26376782ccf1274a49772352ebe1
TobiasMu <64594392+TobiasMu@users.noreply.github.com>
2024-06-18 17:06:43 +0200

    commit again

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,10 +14,9 @@ class ModelTests(TestCase):
         email = "test@example.com"
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password
+            email=email,
+            password=password
         )
-
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -34,11 +33,10 @@ class ModelTests(TestCase):
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
-
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('','test123')
+            get_user_model().objects.create_user('', 'test123')
 
     def test_create_super_user(self):
         """Test creating a superuser."""
